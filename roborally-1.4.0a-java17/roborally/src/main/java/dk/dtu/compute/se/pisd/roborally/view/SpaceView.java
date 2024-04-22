bbe832d1255d5e63b918be3882f325b0d32f2134
victor mazanti <victormazanti@hotmail.com>
2024-04-22 14:28:38 +0200

    order of ui changed

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -95,9 +95,10 @@ public class SpaceView extends StackPane implements ViewObserver {
     @Override
     public void updateView(Subject subject) {
         if (subject == this.space) {
+            updatePlayer();
             updatePit();
             updateReboot();
-            updatePlayer();
+
         }
     }
 
