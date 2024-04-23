a7dba8589711cb3de12b468bda58a6a50a6d9377
karlpedro <162471444+karlpedro@users.noreply.github.com>
2024-04-23 15:55:33 +0200

    updated playerspawns after merge

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -87,10 +87,11 @@ public class AppController implements Observer {
             Board board = new Board(5 ,5);
             gameController = new GameController(board);
             int no = result.get();
+            int[] startPoints = new int[]{0, 2, 3, 6, 7, 9};
             for (int i = 0; i < no; i++) {
                 Player player = new Player(board, PLAYER_COLORS.get(i), "Player " + (i + 1));
                 board.addPlayer(player);
-                player.setSpace(board.getSpace(i % board.width, i));
+                player.setSpace(board.getSpace(0, startPoints[i]));
             }
 
             // XXX: V2
