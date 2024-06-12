eb3ad8558fcb4401a6fc6f708c70b895196c4433
Balder Jacobsen <62349816+TheRealChair@users.noreply.github.com>
2024-06-12 10:26:36 +0200

    Fix shitty sql

--- roborally-1.4.0a-java17/roborally/target/classes/data.sql
+++ roborally-1.4.0a-java17/roborally/target/classes/data.sql
@@ -1,7 +1,6 @@
 -- Insert data into games table with a unique game_id value
-INSERT INTO games (turn_id) VALUES
-(1);
-
+INSERT INTO games (no_of_players, board_id) VALUES
+(1, 1);
 -- Insert data into players table with references to the game_id from games table
-INSERT INTO players (game_id, 1) VALUES
+INSERT INTO players (game_id, game_player_id) VALUES
 (1, 1);
