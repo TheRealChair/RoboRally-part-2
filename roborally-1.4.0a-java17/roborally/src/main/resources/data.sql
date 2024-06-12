2aaaeec615afdb37ccff4c357404d10b41c70d21
Balder Jacobsen <62349816+TheRealChair@users.noreply.github.com>
2024-06-12 10:18:44 +0200

    Arhh

--- roborally-1.4.0a-java17/roborally/src/main/resources/data.sql
+++ roborally-1.4.0a-java17/roborally/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 -- Insert data into games table with a unique game_id value
-INSERT INTO games (turn_id) VALUES (1);
+INSERT INTO games (turn_id) VALUES
+(1);
 
 -- Insert data into players table with references to the game_id from games table
-INSERT INTO players (player_name, score, game_id) VALUES
-('Player1', 0, 1);
+INSERT INTO players (game_id, 1) VALUES
+(1, 1);
